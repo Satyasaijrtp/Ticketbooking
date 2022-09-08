7b77857699dc0cdeef0a6809eceb99ab7258f01c
Satyasaijrtp <112158994+Satyasaijrtp@users.noreply.github.com>
2022-09-08 18:28:30 +0530

    Update Application.java

--- src/main/java/com/info/inet/Application.java
+++ src/main/java/com/info/inet/Application.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class Application {
-
+System.out.println("Ticketbooking");
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
